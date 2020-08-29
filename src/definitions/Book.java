91df3e79bbe4479229a52b717202609d799065fc
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-08-29 22:29:35 +0530

    create a non-parameterized constructor for the Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -4,4 +4,10 @@ public class Book {
     private String bookName;
     private String authorName;
     private String ISBNCode;
+
+    public Book() {
+        this.bookName = "Let us Java";
+        this.authorName = "Yashwant Kanetkar";
+        this.ISBNCode = "978-93-5176-471-7";
+    }
 }
