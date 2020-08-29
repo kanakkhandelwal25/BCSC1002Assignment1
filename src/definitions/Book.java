b4074c9e199fdb93a1817d51082033b0de8a667d
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-08-29 22:49:34 +0530

    add setter methods for private fields of Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -21,12 +21,24 @@ public class Book {
         return bookName;
     }
 
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
     public String getAuthorName() {
         return authorName;
     }
 
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
     public String getISBNCode() {
         return ISBNCode;
+    }
+
+    public void setISBNCode(String ISBNCode) {
+        this.ISBNCode = ISBNCode;
     }
 
 }
