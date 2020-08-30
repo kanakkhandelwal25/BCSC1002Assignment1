ca8971298a537eea8383331630d83d9c01762c1a
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-08-30 22:17:33 +0530

    add non-parameterized constructor for the Library class

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -3,4 +3,11 @@ package definitions;
 
 public class Library {
     private Book[] booksAvailable;
+
+    public Library() {
+        this.booksAvailable = new Book[5];
+        for (int i = 0; i < booksAvailable.length; i++) {
+            booksAvailable[i] = new Book();
+        }
+    }
 }
