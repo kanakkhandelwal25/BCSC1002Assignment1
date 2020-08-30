bf2ac11a93a3ce7a72021446029eeaab75c5a471
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2020-08-30 19:35:24 +0530

    add private fields for the Student class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,11 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
- *  File Name : Student.java
- * */
+
 package definitions;
 
 public class Student {
+
+    private String studentName;
+    private long rollNumber;
+    private int noOfBooksIssued;
+    private String[] booksIssued;
+
 }
